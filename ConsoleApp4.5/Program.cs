f9dad1a292c3c76456896f5718aedac6766a3dec
D1SS007 <101556566+D1SS007@users.noreply.github.com>
2022-04-12 20:43:03 +0300

    Update Program.cs

--- ConsoleApp4.5/Program.cs
+++ ConsoleApp4.5/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ConsoleApp4._5
 {
@@ -14,34 +14,36 @@ namespace ConsoleApp4._5
             int[] ints = Array.ConvertAll(parts, int.Parse); 
 
             Console.WriteLine("Массив до тасовки");
-            
-            for (int i = 0; i < ints.Length; i++)
-            {
-                 Console.Write(ints[i] + " ");
-            }
-            Console.WriteLine();
+
+            DrawMassive(ints);
 
             Shuffle(ref ints);
-        }
-
-        static void Shuffle(ref int[] suffleMassive)
-        {
-            Random random = new Random();
-            for(int i = suffleMassive.Length - 1; i >= 1 ; i--)
-            {
-                int j = random.Next(0, i + 1);
-                var temp = suffleMassive[j];                
-                suffleMassive[j] = suffleMassive [i];
-                suffleMassive[i] = temp;
-            }
 
             Console.WriteLine("Массив после тасовки");
 
-            for (int i = 0; i < suffleMassive.Length; i++)
-            {
-                Console.Write(suffleMassive[i] + " ");
-            }
+            DrawMassive(ints);
+        }
 
+        static void Shuffle(ref int[] shuffleMassive)
+        {
+            Random random = new Random();
+            
+            for(int i = shuffleMassive.Length - 1; i >= 1 ; i--)
+            {
+                int j = random.Next(0, i + 1);
+                var temp = shuffleMassive[j];                
+                shuffleMassive[j] = shuffleMassive [i];
+                shuffleMassive[i] = temp;
+            }
+        }
+
+        static void DrawMassive(int [] massive)
+        {
+            for (int i = 0; i < massive.Length; i++)
+            {
+                Console.Write(massive[i] + " ");
+            }
+            
             Console.WriteLine();
         }
     }
